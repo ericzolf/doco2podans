adbf1fbc91d6f035e0c8cd565499a9d621c26f85
Eric L <ewl+git@lavar.de>
2022-02-17 07:06:11 +0100

    Shuffle around the gitlab ports to make sample work
    GitLab uses 8080 internally and that generated a conflict

--- samples/gitlab/gitlab-insecure.rb
+++ samples/gitlab/gitlab-insecure.rb
@@ -1,3 +1,3 @@
-external_url 'http://localhost.localdomain:8080/'
+external_url 'http://localhost.localdomain:8888/'
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password')
 gitlab_rails['gitlab_shell_ssh_port'] = 2222
